fafa6b8016232f08d22a03e5a0267d49586e4acd
Milton Soares Filho <milton.sfilho@telefonica.com>
2016-05-24 18:40:27 -0300

    br-external basic structure
    Signed-off-by: Milton Soares Filho <milton.sfilho@telefonica.com>

---
+++ br-external/package/mapbox/mapbox.mk
@@ -0,0 +1,3 @@
+include $(BR2_EXTERNAL)/package/mapbox/geometryhpp/geometryhpp.mk
+include $(BR2_EXTERNAL)/package/mapbox/nunicode/nunicode.mk
+include $(BR2_EXTERNAL)/package/mapbox/qmapboxgl/qmapboxgl.mk
