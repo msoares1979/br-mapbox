ad4a8a6b3ea2a7c1ad6918644672f9005b319eca
Milton Soares Filho <milton.soares.filho@gmail.com>
2016-05-27 15:19:53 -0300

    script to auto-configure project
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

---
+++ configure.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -f
+
+fetch_buildroot ()
+{
+	wget https://buildroot.org/downloads/buildroot-2016.02.tar.gz
+	tar xjvf buildroot-2016.02.tar.gz
+	mv buildroot-2016.02 buildroot
+}
+
+do_configure ()
+{
+	make BR2_EXTERNAL=$PWD/br-external -C buildroot O=$PWD/output/fancyboard myfancyboard_defconfig
+}
+
+if [ ! -d buildroot ]; then
+	fetch_buildroot
+fi
+
+do_configure
