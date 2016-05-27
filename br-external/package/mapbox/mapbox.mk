61ebbac5593fe4997f58fc791aae0a06d191839e
Milton Soares Filho <milton.soares.filho@gmail.com>
2016-05-27 15:21:39 -0300

    include new packages
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- br-external/package/mapbox/mapbox.mk
+++ br-external/package/mapbox/mapbox.mk
@@ -1,3 +1,7 @@
 include $(BR2_EXTERNAL)/package/mapbox/geometryhpp/geometryhpp.mk
 include $(BR2_EXTERNAL)/package/mapbox/nunicode/nunicode.mk
+include $(BR2_EXTERNAL)/package/mapbox/variant/variant.mk
+include $(BR2_EXTERNAL)/package/mapbox/protozero/protozero.mk
+include $(BR2_EXTERNAL)/package/mapbox/geojsonvt/geojsonvt.mk
+include $(BR2_EXTERNAL)/package/mapbox/rapidjson/rapidjson.mk
 include $(BR2_EXTERNAL)/package/mapbox/qmapboxgl/qmapboxgl.mk
