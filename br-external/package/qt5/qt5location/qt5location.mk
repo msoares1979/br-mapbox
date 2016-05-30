56103a5ac97a0686cc8dae71bd76dd21d6ed1437
Milton Soares Filho <milton.soares.filho@gmail.com>
2016-05-30 19:06:07 -0300

    qt5location: integrate buildroot 2016-05 fixes
    Signed-off-by: Milton Soares Filho <milton.soares.filho@gmail.com>

--- br-external/package/qt5/qt5location/qt5location.mk
+++ br-external/package/qt5/qt5location/qt5location.mk
@@ -18,6 +18,10 @@ QT5LOCATION_LICENSE = Commercial license
 QT5LOCATION_REDISTRIBUTE = NO
 endif
 
+ifeq ($(BR2_PACKAGE_QT5DECLARATIVE),y)
+QT5LOCATION_DEPENDENCIES += qt5declarative
+endif
+
 define QT5LOCATION_CONFIGURE_CMDS
 	(cd $(@D); $(TARGET_MAKE_ENV) $(HOST_DIR)/usr/bin/qmake)
 endef
@@ -31,21 +35,26 @@ define QT5LOCATION_INSTALL_STAGING_CMDS
 	$(QT5_LA_PRL_FILES_FIXUP)
 endef
 
-ifeq ($(BR2_STATIC_LIBS),)
-
-define QT5LOCATION_INSTALL_TARGET_CMDS
-	cp -dpf $(STAGING_DIR)/usr/lib/libQt5Location*.so.* $(TARGET_DIR)/usr/lib
-	cp -dpf $(STAGING_DIR)/usr/lib/libQt5Positioning*.so.* $(TARGET_DIR)/usr/lib
-	cp -dpfr $(STAGING_DIR)/usr/lib/qt/plugins/* $(TARGET_DIR)/usr/lib/qt/plugins
-endef
-
 ifeq ($(BR2_PACKAGE_QT5DECLARATIVE_QUICK),y)
-define QT5MULTIMEDIA_INSTALL_TARGET_QMLS
-	cp -dpfr $(STAGING_DIR)/usr/qml/QtLocation $(TARGET_DIR)/usr/qml/
+define QT5LOCATION_INSTALL_TARGET_QMLS
 	cp -dpfr $(STAGING_DIR)/usr/qml/QtPositioning $(TARGET_DIR)/usr/qml/
+	cp -dpfr $(STAGING_DIR)/usr/qml/QtLocation $(TARGET_DIR)/usr/qml/
+endef
+define QT5LOCATION_INSTALL_TARGET_LOCATION
+	cp -dpf $(STAGING_DIR)/usr/lib/libQt5Location.so.* $(TARGET_DIR)/usr/lib
+	cp -dpfr $(STAGING_DIR)/usr/lib/qt/plugins/geoservices $(TARGET_DIR)/usr/lib/qt/plugins/
 endef
 endif
 
-endif # BR2_STATIC_LIBS
+define QT5LOCATION_INSTALL_TARGET_POSITION
+	cp -dpf $(STAGING_DIR)/usr/lib/libQt5Positioning.so.* $(TARGET_DIR)/usr/lib
+	cp -dpfr $(STAGING_DIR)/usr/lib/qt/plugins/position $(TARGET_DIR)/usr/lib/qt/plugins/
+endef
+
+define QT5LOCATION_INSTALL_TARGET_CMDS
+	$(QT5LOCATION_INSTALL_TARGET_POSITION)
+	$(QT5LOCATION_INSTALL_TARGET_LOCATION)
+	$(QT5LOCATION_INSTALL_TARGET_QMLS)
+endef
 
 $(eval $(generic-package))
